b30a428189b6da84cce6181bf79efdaf6f046b74
krishnaparayil <62947302+krishnaparayil@users.noreply.github.com>
2020-03-31 20:21:27 +0530

    Create Learn to code.c

---
+++ Learn to code.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("Krishna P");
+  printf(".College of engineering Chengannur");
+  printf(".Electrical and electronics engineering");
+  return 0;
+} 
